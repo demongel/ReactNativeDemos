ae0fe9fe781022af6b9b71a6dbe2e4f013b4e129
Zhuang <zez160089@163.com>
2018-03-13 17:47:19 +0800

    Update App.js
    默认初始化的文件

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
@@ -9,60 +9,29 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
-  AppRegistry,
-	Dimensions,
-  PixelRatio,
-  TextInput
+  View
 } from 'react-native';
 
-const {height,width} = Dimensions.get('window');
-const pixelRatio = PixelRatio.get();
-let widthOfMargin = Dimensions.get('window').width*0.05;
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
 type Props = {};
 export default class App extends Component<Props> {
-  constructor(props){
-    super(props);
-    this.state = { 
-      inputNum:'',
-      password:''  
-    };
-  }
-
-  updateNum(newNum){
-    this.setState((state) => {
-      console.log('update has been exec');
-      return {
-        inputNum:newNum,
-      };});
-  }
-
-  updatePW(newPW){
-    this.setState(
-      () => {
-        return {password : newPW,};
-      }
-    );
-  }
-
-  render() {    
-    console.log('render exec');
+  render() {
     return (
-      <View style = {styles.container}>
-        <TextInput style = {styles.textInputStyle}
-          placeholder = {'请输入手机号'} 
-          onChangeText = {(newNum)=>this.updateNum(newNum)}
-          />
-        <Text style = {styles.textStyle}>
-           输入的手机号：{this.state.inputNum}
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
         </Text>
-        <TextInput style = {styles.textInputStyle}
-          placeholder = {'请输入密码'}
-          password = {true}
-          />
-        <Text style = {styles.bigTextStyle}>
-          确定
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
         </Text>
       </View>
     );
@@ -72,28 +41,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',// 内部的控件会居中
-    // alignItems: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  textInputStyle: {
+  welcome: {
     fontSize: 20,
-    textAlign: 'center',  // 文字会居中
-    backgroundColor: 'yellow',
-    margin: widthOfMargin,
+    textAlign: 'center',
+    margin: 10,
   },
-  textStyle: {
-    margin: widthOfMargin,
-    // textAlign: 'center',
+  instructions: {
+    textAlign: 'center',
     color: '#333333',
-    marginBottom: 10,
-    fontSize:18,
+    marginBottom: 5,
   },
-  bigTextStyle:{
-    margin:widthOfMargin,
-    backgroundColor:'green',  // 背景颜色
-    textAlign:'center',
-    color:'red',  // 文字颜色
-    fontSize:30   //文字尺寸
-  }
 });
